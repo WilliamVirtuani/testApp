afd0a72b4fe343bcd0a336037e6b5dbd9431455a
WilliamV94 <w.virtuani.ita@be-tse.it>
2021-11-09 17:46:20 +0100

    CONFIGURATION: Routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,34 @@ import "./App.css";
 import styled from "styled-components";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { isJSDocParameterTag } from "typescript";
 
 const WelcomeLabel = styled.h1`
   color: green;
 `;
 
 function App() {
-  return <WelcomeLabel>Hello World</WelcomeLabel>;
+  let navigate = useNavigate();
+
+  function Home() {
+    return (
+      <div>
+        <WelcomeLabel>HOME!</WelcomeLabel>
+        <button onClick={() => navigate("/about")}>press</button>
+      </div>
+    );
+  }
+
+  function About() {
+    return <WelcomeLabel>ABOUT</WelcomeLabel>;
+  }
+
+  return (
+    <Routes>
+      <Route path="/about" element={About()}></Route>
+      <Route path="/" element={Home()}></Route>
+    </Routes>
+  );
 }
 
 export default App;
